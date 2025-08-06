07749dfa71d371f78ccf76c07728a472e9a9ee9a
Enzo949494 <9812@holbertonstudents.com>
2025-08-06 13:48:13 +0200

    fix pystyle

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -3,18 +3,19 @@
 calculate shape of a matrix
 """
 
+
 def matrix_shape(matrix):
-	"""
-	Calculate shape of a matrix
-	
-	Args: matrix list: list representing a matrix
-	
-	Returns: list of integers who represente shape of the matrix
-	"""
-	shape = []
-	while isinstance(matrix, list):
-		shape.append(len(matrix))
-		if len(matrix) == 0:
-			break
-		matrix = matrix[0]
-	return shape
+    """
+    Calculate shape of a matrix
+
+    Args: matrix list: list representing a matrix
+
+    Returns: list of integers who represente shape of the matrix
+    """
+    shape = []
+    while isinstance(matrix, list):
+        shape.append(len(matrix))
+        if len(matrix) == 0:
+            break
+        matrix = matrix[0]
+    return shape
