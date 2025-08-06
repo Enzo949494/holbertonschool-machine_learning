76a87f57b7331374ffa51f65862a296fda456627
Enzo949494 <9812@holbertonstudents.com>
2025-08-06 13:45:21 +0200

    task2, shape of matrix

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""""
+calculate shape of a matrix
+"""
+
+def matrix_shape(matrix):
+	"""
+	Calculate shape of a matrix
+	
+	Args: matrix list: list representing a matrix
+	
+	Returns: list of integers who represente shape of the matrix
+	"""
+	shape = []
+	while isinstance(matrix, list):
+		shape.append(len(matrix))
+		if len(matrix) == 0:
+			break
+		matrix = matrix[0]
+	return shape
