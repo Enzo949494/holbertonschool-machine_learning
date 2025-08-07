e4417684b10941f4c7ed7ca825d39102d58b0fa5
Enzo949494 <9812@holbertonstudents.com>
2025-08-07 13:14:58 +0200

    task12, fix pystyle

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -7,10 +7,10 @@ Element-wise operations with numpy
 def np_elementwise(mat1, mat2):
     """
     Element-wise +, -, *, /
-    
+
     Args:mat1: 1st numpy array
          mat2: 2nd numpy array or scalar
-    
+
     Returns:tuple containing (sum, difference, product, quotient)
     """
     return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
