62d0d6432e6be1b91ae7beca1806703ab7765eae
Enzo949494 <9812@holbertonstudents.com>
2025-08-07 13:46:49 +0200

    task14, matrix multiplication numpy

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Matrix multiplication with numpy
+"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """
+    Matrix multiplication
+
+    Args:mat1: 1st numpy array
+         mat2: 2nd numpy array
+
+    Returns:new numpy.ndarray
+    """
+    return np.matmul(mat1, mat2)
