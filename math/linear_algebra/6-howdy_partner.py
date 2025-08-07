f30e03a87eaedb19d7a5e6700b01c188c02153f6
Enzo949494 <9812@holbertonstudents.com>
2025-08-07 11:58:31 +0200

    task6, concatenate 2 array

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Concatenate two arrays
+"""
+
+
+def cat_arrays(arr1, arr2):
+    """
+    Concatenate 2 arrays and return a new list
+
+    Args: arr1 & 2 -> int/float
+
+    Returns: concatenated arr1 & 2
+    """
+    return arr1 + arr2
