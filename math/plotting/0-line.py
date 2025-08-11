d8614a9f1e5a9bc86a4bb33712c3589024029f94
Enzo949494 <9812@holbertonstudents.com>
2025-08-11 14:26:04 +0200

    task0, curve red y=x**3

---
+++ math/plotting/0-line.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def line():
+    """
+    plot the curve y 0->10
+    solid red line, x limited 0 to 10
+    """
+    y = np.arange(0, 11) ** 3
+    plt.figure(figsize=(6.4, 4.8))
+
+    x = np.arange(0, 11)
+    plt.plot(x, y, 'r-')
+    plt.xlim(0, 10)
+    plt.show()
