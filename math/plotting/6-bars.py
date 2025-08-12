5f6217f5fac9886c31ed41d177b8060821237001
Enzo949494 <9812@holbertonstudents.com>
2025-08-12 15:29:56 +0200

    task6, fix pystyle

--- math/plotting/6-bars.py
+++ math/plotting/6-bars.py
@@ -1,32 +1,39 @@
 #!/usr/bin/env python3
+"""
+plot bar
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def bars():
+    """
+    plot bar
+    """
     np.random.seed(5)
-    fruit = np.random.randint(0, 20, (4,3))
+    fruit = np.random.randint(0, 20, (4, 3))
     plt.figure(figsize=(6.4, 4.8))
 
-    
     people = ['Farrah', 'Fred', 'Felicia']
     fruits = ['apples', 'bananas', 'oranges', 'peaches']
     colors = ['red', 'yellow', '#ff8000', '#ffe5b4']
-    
+
     x = np.arange(len(people))
     width = 0.5
-    
+
     bottom = np.zeros(len(people))
-    
+
     for i in range(len(fruits)):
-        plt.bar(x, fruit[i], width, bottom=bottom, color=colors[i], label=fruits[i])
+        plt.bar(x, fruit[i], width, bottom=bottom, color=colors[i],
+                label=fruits[i])
         bottom += fruit[i]
-    
+
     plt.xlabel('Person')
     plt.ylabel('Quantity of Fruit')
     plt.title('Number of Fruit per Person')
     plt.xticks(x, people)
     plt.yticks(range(0, 81, 10))
     plt.ylim(0, 80)
-    
+
     plt.legend()
     plt.show()
