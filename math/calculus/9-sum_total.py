04dfb0ed0bde53b3e64fee0db3c8c503e4fe28e0
Enzo949494 <9812@holbertonstudents.com>
2025-08-13 13:28:26 +0200

    task9, fix pystyle

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -5,9 +5,9 @@ sigma fonction
 
 
 def summation_i_squared(n):
-	"""
-	code for sigma
-	"""
-	if not isinstance(n, int) or n < 1:
-		return None
-	return sum(i**2 for i in range(1, n+1))
+    """
+    code for sigma
+    """
+    if not isinstance(n, int) or n < 1:
+        return None
+    return sum(i**2 for i in range(1, n+1))
