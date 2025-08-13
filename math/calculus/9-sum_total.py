ae174279e182ac6158b064bd6471a7373fa60868
Enzo949494 <9812@holbertonstudents.com>
2025-08-13 13:27:42 +0200

    task9, python sigma

---
+++ math/calculus/9-sum_total.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+sigma fonction
+"""
+
+
+def summation_i_squared(n):
+	"""
+	code for sigma
+	"""
+	if not isinstance(n, int) or n < 1:
+		return None
+	return sum(i**2 for i in range(1, n+1))
