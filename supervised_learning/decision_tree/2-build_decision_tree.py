bae6f43a0c2b4a73ad7c9457f0b9fa3dc41be0a5
Enzo949494 <9812@holbertonstudents.com>
2025-08-20 13:11:45 +0200

    task2

--- supervised_learning/decision_tree/2-build_decision_tree.py
+++ supervised_learning/decision_tree/2-build_decision_tree.py
@@ -89,10 +89,10 @@ class Node:
         Returns:
             str: Formatted text with left child prefix
         """
-        lines = text.split('\n')
-        new_text = "    +---> " + lines[0] + "\n"
+        lines = text.split("\n")
+        new_text = "    +--" + lines[0] + "\n"
         for x in lines[1:]:
-            new_text += "    |      " + x + "\n"
+            new_text += ("    |  " + x) + "\n"
         return new_text
 
     def right_child_add_prefix(self, text):
@@ -105,10 +105,10 @@ class Node:
         Returns:
             str: Formatted text with right child prefix
         """
-        lines = text.split('\n')
-        new_text = "    +---> " + lines[0] + "\n"
+        lines = text.split("\n")
+        new_text = "    +--" + lines[0] + "\n"
         for x in lines[1:]:
-            new_text += "           " + x + "\n"  # 11 espaces !
+            new_text += ("       " + x) + "\n"
         return new_text
 
     def __str__(self):
