6b5ce9adb2f825529dfe17311ef2aa6273278790
Enzo949494 <9812@holbertonstudents.com>
2025-08-20 12:47:52 +0200

    task2, ttf

--- supervised_learning/decision_tree/2-build_decision_tree.py
+++ supervised_learning/decision_tree/2-build_decision_tree.py
@@ -92,7 +92,7 @@ class Node:
         lines = text.split('\n')
         new_text = "    +---> " + lines[0] + "\n"
         for x in lines[1:]:
-            new_text += "   |   " + x + "\n"
+            new_text += "    |   " + x + "\n"
         return new_text
 
     def right_child_add_prefix(self, text):
