ff565ce9c12fd1abd4aae404e9a770cae22b57c7
Enzo949494 <9812@holbertonstudents.com>
2025-08-28 16:48:27 +0200

    task11, fix pystyle

--- supervised_learning/classification/11-neural_network.py
+++ supervised_learning/classification/11-neural_network.py
@@ -5,16 +5,17 @@ import numpy as np
 
 
 class NeuralNetwork:
-    """Class that defines a neural network with one hidden layer performing binary classification"""
+    """Class that defines a neural network with one
+       hidden layer performing binary classification"""
 
     def __init__(self, nx, nodes):
         """
         Constructor for NeuralNetwork class
-        
+
         Args:
             nx: number of input features
             nodes: number of nodes found in the hidden layer
-            
+
         Raises:
             TypeError: if nx is not an integer
             ValueError: if nx is less than 1
@@ -29,7 +30,7 @@ class NeuralNetwork:
             raise TypeError("nodes must be an integer")
         if nodes < 1:
             raise ValueError("nodes must be a positive integer")
-            
+
         # Initialize private attributes
         self.__W1 = np.random.normal(size=(nodes, nx))
         self.__b1 = np.zeros((nodes, 1))
@@ -71,44 +72,44 @@ class NeuralNetwork:
     def forward_prop(self, X):
         """
         Calculates the forward propagation of the neural network
-        
+
         Args:
             X: numpy.ndarray with shape (nx, m) that contains the input data
                nx is the number of input features to the neuron
                m is the number of examples
-               
+
         Returns:
             The private attributes __A1 and __A2, respectively
         """
         z1 = np.dot(self.__W1, X) + self.__b1
-        
+
         self.__A1 = 1 / (1 + np.exp(-z1))
-        
+
         z2 = np.dot(self.__W2, self.__A1) + self.__b2
-        
+
         self.__A2 = 1 / (1 + np.exp(-z2))
-        
+
         return self.__A1, self.__A2
 
     def cost(self, Y, A):
         """
         Calculates the cost of the model using logistic regression
-        
+
         Args:
             Y: numpy.ndarray with shape (1, m) that contains the correct labels
                for the input data
             A: numpy.ndarray with shape (1, m) containing the activated output
                of the neuron for each example
-               
+
         Returns:
             The cost
         """
         # Get number of examples
         m = Y.shape[1]
-        
+
         # Calculate logistic regression cost
         # Cost = -1/m * sum(Y*log(A) + (1-Y)*log(1-A))
         # Using 1.0000001 - A instead of 1 - A to avoid division by zero
         cost = -1/m * np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
-        
+
         return cost
