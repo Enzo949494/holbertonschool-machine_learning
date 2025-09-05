05ff56e31cd0ce980879629385973b6278c60484
Enzo949494 <9812@holbertonstudents.com>
2025-09-05 13:27:16 +0200

    task4, fix pystyle

--- supervised_learning/keras/4-train.py
+++ supervised_learning/keras/4-train.py
@@ -4,18 +4,19 @@
 import tensorflow.keras as K
 
 
-def train_model(network, data, labels, batch_size, epochs, verbose=True, shuffle=False):
+def train_model(network, data, labels, batch_size, epochs,
+                verbose=True, shuffle=False):
     """
     Trains a model using mini-batch gradient descent
 
     Args:
         network: the model to train
         data: numpy.ndarray of shape (m, nx) containing the input data
-        labels: one-hot numpy.ndarray of shape (m, classes) containing the labels
+        labels: one-hot numpy.ndarray of shape (m, classes) contain the labels
         batch_size: size of the batch used for mini-batch gradient descent
         epochs: number of passes through data for mini-batch gradient descent
-        verbose: boolean that determines if output should be printed during training
-        shuffle: boolean that determines whether to shuffle the batches every epoch
+        verbose: bool that determines if output should be printed during train
+        shuffle: bool that determines whether to shuffle the batche every epoch
 
     Returns:
         The History object generated after training the model
@@ -26,9 +27,9 @@ def train_model(network, data, labels, batch_size, epochs, verbose=True, shuffle
         labels,                 # Target labels (Y_train_oh)
         batch_size=batch_size,  # Size of each batch for gradient descent
         epochs=epochs,          # Number of complete passes through the dataset
-        verbose=verbose,        # Print training progress (True) or silent (False)
-        shuffle=shuffle         # Shuffle data between epochs (False for reproducibility)
+        verbose=verbose,        # Print train progress (True) or silent (False)
+        shuffle=shuffle         # Shuffle data btwn epoch (Fals for reproduc)
     )
-    
+
     # Return the History object containing training metrics
     return history
