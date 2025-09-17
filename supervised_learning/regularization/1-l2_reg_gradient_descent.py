b7d43380022d6681f3920e1418b4a5fa4db45a77
Enzo949494 <9812@holbertonstudents.com>
2025-09-17 11:50:58 +0200

    task1, fix pystyle

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -8,7 +8,7 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     """
     Updates the weights and biases of a neural network using gradient descent
     with L2 regularization
-    
+
     Args:
         Y: one-hot numpy.ndarray of shape (classes, m) with correct labels
         weights: dictionary of weights and biases of the neural network
@@ -18,25 +18,26 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
         L: number of layers of the network
     """
     m = Y.shape[1]
-    
+
     # Start with output layer (softmax)
     dZ = cache[f'A{L}'] - Y
-    
+
     for layer in range(L, 0, -1):
         # Current layer activations
         A_prev = cache[f'A{layer-1}']
-        
+
         # Gradients for weights and biases WITH L2 regularization
-        dW = (1/m) * np.matmul(dZ, A_prev.T) + (lambtha/m) * weights[f'W{layer}']
+        dW = ((1/m) * np.matmul(dZ, A_prev.T) +
+              (lambtha/m) * weights[f'W{layer}'])
         db = (1/m) * np.sum(dZ, axis=1, keepdims=True)
-        
+
         # Calculate dZ for previous layer BEFORE updating weights
         if layer > 1:
             # dA for previous layer (using current weights)
             dA_prev = np.matmul(weights[f'W{layer}'].T, dZ)
             # dZ for previous layer (tanh derivative: 1 - tanh²(x))
             dZ = dA_prev * (1 - cache[f'A{layer-1}']**2)
-        
+
         # Update weights and biases AFTER calculating dZ for next iteration
         weights[f'W{layer}'] -= alpha * dW
         weights[f'b{layer}'] -= alpha * db
