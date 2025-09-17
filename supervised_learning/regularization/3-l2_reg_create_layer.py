012a3b51b6d8fbec96a6e3e3689e0bb4cfe17bb3
Enzo949494 <9812@holbertonstudents.com>
2025-09-17 15:23:42 +0200

    task3,fix with fan avg

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -1,20 +1,24 @@
-#!/usr/bin/env python3
-"""L2 Regularization Layer Creation with TensorFlow"""
-
 import tensorflow as tf
-
 
 def l2_reg_create_layer(prev, n, activation, lambtha):
     """
-    Creates a neural network layer in TensorFlow that includes L2 regularization
+    Creates a dense neural network layer with L2 regularization and
+    VarianceScaling weight initialization.
+
+    Args:
+        prev: tensor output from previous layer
+        n: number of nodes for the new layer
+        activation: activation function to apply
+        lambtha: L2 regularization parameter
+
+    Returns:
+        tensor output of the new layer with L2 regularization
     """
-    regularizer = tf.keras.regularizers.l2(lambtha) if lambtha > 0 else None
     layer = tf.keras.layers.Dense(
         units=n,
         activation=activation,
-        kernel_regularizer=regularizer,
-        kernel_initializer=tf.keras.initializers.VarianceScaling(
-            scale=2.0, mode='fan_avg'
-        )
-    )
-    return layer(prev)
+        kernel_regularizer=tf.keras.regularizers.L2(l2=lambtha),
+        kernel_initializer=tf.keras.initializers.VarianceScaling(scale=2.0, mode='fan_avg')
+    )(prev)
+
+    return layer
