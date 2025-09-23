1549db3f030824442d214c6cef06e2d0da9b8112
Enzo949494 <9812@holbertonstudents.com>
2025-09-23 13:49:40 +0200

    task3, back to first code

--- math/convolutions_and_pooling/3-convolve_grayscale.py
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
 """General grayscale convolution with padding and stride"""
+
 import numpy as np
 
 def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     """
     Performs a convolution on grayscale images with custom padding and stride.
+
     Args:
         images: numpy.ndarray with shape (m, h, w)
         kernel: numpy.ndarray with shape (kh, kw)
         padding: tuple of (ph, pw), 'same', or 'valid'
         stride: tuple of (sh, sw)
+
     Returns:
         numpy.ndarray containing the convolved images
     """
@@ -20,38 +23,24 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     # Determine padding
     if type(padding) is tuple:
         ph, pw = padding
-        pad_top = ph // 2
-        pad_bottom = ph - pad_top
-        pad_left = pw // 2
-        pad_right = pw - pad_left
     elif padding == 'same':
-        # Compute total padding needed on height and width
-        ph_total = max((h - 1) * sh + kh - h, 0)
-        pw_total = max((w - 1) * sw + kw - w, 0)
-        # Distribute padding to top/bottom and left/right
-        pad_top = ph_total // 2
-        pad_bottom = ph_total - pad_top
-        pad_left = pw_total // 2
-        pad_right = pw_total - pad_left
+        ph = ((h - 1) * sh + kh - h) // 2
+        pw = ((w - 1) * sw + kw - w) // 2
     elif padding == 'valid':
-        pad_top = pad_bottom = pad_left = pad_right = 0
+        ph, pw = 0, 0
     else:
         raise ValueError("padding must be 'same', 'valid', or a tuple of (ph, pw)")
 
     # Pad images
     images_padded = np.pad(
         images,
-        ((0, 0), (pad_top, pad_bottom), (pad_left, pad_right)),
+        ((0, 0), (ph, ph), (pw, pw)),
         mode='constant'
     )
 
     # Output dimensions
-    if padding == 'valid':
-        out_h = (h - kh) // sh + 1
-        out_w = (w - kw) // sw + 1
-    else:
-        out_h = ((h + pad_top + pad_bottom - kh) // sh) + 1
-        out_w = ((w + pad_left + pad_right - kw) // sw) + 1
+    out_h = ((h + 2 * ph - kh) // sh) + 1
+    out_w = ((w + 2 * pw - kw) // sw) + 1
 
     output = np.zeros((m, out_h, out_w))
 
