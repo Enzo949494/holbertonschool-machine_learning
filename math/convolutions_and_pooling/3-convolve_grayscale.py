7b0e8c485483facc7d77da82f72a5411338e6567
Enzo949494 <9812@holbertonstudents.com>
2025-09-23 13:05:46 +0200

    task3, stride

---
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""General grayscale convolution with padding and stride"""
+
+import numpy as np
+
+def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
+    """
+    Performs a convolution on grayscale images with custom padding and stride.
+
+    Args:
+        images: numpy.ndarray with shape (m, h, w)
+        kernel: numpy.ndarray with shape (kh, kw)
+        padding: tuple of (ph, pw), 'same', or 'valid'
+        stride: tuple of (sh, sw)
+
+    Returns:
+        numpy.ndarray containing the convolved images
+    """
+    m, h, w = images.shape
+    kh, kw = kernel.shape
+    sh, sw = stride
+
+    # Determine padding
+    if type(padding) is tuple:
+        ph, pw = padding
+    elif padding == 'same':
+        ph = ((h - 1) * sh + kh - h) // 2
+        pw = ((w - 1) * sw + kw - w) // 2
+    elif padding == 'valid':
+        ph, pw = 0, 0
+    else:
+        raise ValueError("padding must be 'same', 'valid', or a tuple of (ph, pw)")
+
+    # Pad images
+    images_padded = np.pad(
+        images,
+        ((0, 0), (ph, ph), (pw, pw)),
+        mode='constant'
+    )
+
+    # Output dimensions
+    out_h = ((h + 2 * ph - kh) // sh) + 1
+    out_w = ((w + 2 * pw - kw) // sw) + 1
+
+    output = np.zeros((m, out_h, out_w))
+
+    for i in range(out_h):
+        for j in range(out_w):
+            i_start = i * sh
+            j_start = j * sw
+            region = images_padded[:, i_start:i_start+kh, j_start:j_start+kw]
+            output[:, i, j] = np.sum(region * kernel, axis=(1, 2))
+
+    return output
