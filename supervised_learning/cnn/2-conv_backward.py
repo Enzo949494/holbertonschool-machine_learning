0e702d9fc4712878a161719d2d1aa140ba27f835
Enzo949494 <9812@holbertonstudents.com>
2025-09-30 16:06:20 +0200

    task2, ttf mismatch

--- supervised_learning/cnn/2-conv_backward.py
+++ supervised_learning/cnn/2-conv_backward.py
@@ -8,24 +8,24 @@ def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
     m, h_new, w_new, c_new = dZ.shape
     sh, sw = stride
 
-    # Padding dimensions
+    # Calcul du padding
     if padding == 'same':
         pad_h = ((h_prev - 1) * sh + kh - h_prev) // 2
         pad_w = ((w_prev - 1) * sw + kw - w_prev) // 2
     elif padding == 'valid':
         pad_h, pad_w = 0, 0
+    else:
+        raise ValueError("padding must be 'same' or 'valid'")
 
-
-    # Initialize gradients
+    # Initialisation des gradients
     dA_prev = np.zeros_like(A_prev)
     dW = np.zeros_like(W)
     db = np.zeros_like(b)
 
-    # Pad A_prev and dA_prev
+    # Padding des entrées et des gradients d'entrée
     A_prev_pad = np.pad(A_prev, ((0,0), (pad_h, pad_h), (pad_w, pad_w), (0,0)), mode='constant')
     dA_prev_pad = np.pad(dA_prev, ((0,0), (pad_h, pad_h), (pad_w, pad_w), (0,0)), mode='constant')
 
-    # Loop over examples
     for i in range(m):
         a_prev_pad = A_prev_pad[i]
         da_prev_pad = dA_prev_pad[i]
@@ -38,15 +38,14 @@ def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
                     horiz_start = w * sw
                     horiz_end = horiz_start + kw
 
-                    # Slice of the padded input
                     a_slice = a_prev_pad[vert_start:vert_end, horiz_start:horiz_end, :]
 
-                    # Update gradients for the window and filter
+                    # Mise à jour des gradients : 
                     da_prev_pad[vert_start:vert_end, horiz_start:horiz_end, :] += W[:, :, :, c] * dZ[i, h, w, c]
                     dW[:, :, :, c] += a_slice * dZ[i, h, w, c]
                     db[:, :, :, c] += dZ[i, h, w, c]
 
-        # Unpad da_prev_pad to get dA_prev for the current example
+        # Suppression du padding des gradients pour remettre à la bonne taille
         if padding == 'same':
             dA_prev[i, :, :, :] = da_prev_pad[pad_h:-pad_h, pad_w:-pad_w, :]
         else:
