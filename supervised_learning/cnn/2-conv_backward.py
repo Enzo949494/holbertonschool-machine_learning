task2, fix broadcasting

supervised_learning/cnn/2-conv_backward.py
@@ -8,23 +8,18 @@ def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
     m, h_new, w_new, c_new = dZ.shape
     sh, sw = stride
 
-    # Calcul du padding
     if padding == 'same':
         pad_h = ((h_prev - 1) * sh + kh - h_prev) // 2
         pad_w = ((w_prev - 1) * sw + kw - w_prev) // 2
     elif padding == 'valid':
         pad_h, pad_w = 0, 0
-    else:
-        raise ValueError("padding must be 'same' or 'valid'")
 
-    # Initialisation des gradients
     dA_prev = np.zeros_like(A_prev)
     dW = np.zeros_like(W)
     db = np.zeros_like(b)
 
-    # Padding des entrées et des gradients d'entrée
-    A_prev_pad = np.pad(A_prev, ((0,0), (pad_h, pad_h), (pad_w, pad_w), (0,0)), mode='constant')
-    dA_prev_pad = np.pad(dA_prev, ((0,0), (pad_h, pad_h), (pad_w, pad_w), (0,0)), mode='constant')
+    A_prev_pad = np.pad(A_prev, ((0, 0), (pad_h, pad_h), (pad_w, pad_w), (0, 0)), mode='constant')
+    dA_prev_pad = np.pad(dA_prev, ((0, 0), (pad_h, pad_h), (pad_w, pad_w), (0, 0)), mode='constant')
 
     for i in range(m):
         a_prev_pad = A_prev_pad[i]
@@ -40,12 +35,11 @@ def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
 
                     a_slice = a_prev_pad[vert_start:vert_end, horiz_start:horiz_end, :]
 
-                    # Mise à jour des gradients : 
+                    # Update gradients for the window and the filter's parameters
                     da_prev_pad[vert_start:vert_end, horiz_start:horiz_end, :] += W[:, :, :, c] * dZ[i, h, w, c]
                     dW[:, :, :, c] += a_slice * dZ[i, h, w, c]
                     db[:, :, :, c] += dZ[i, h, w, c]
 
-        # Suppression du padding des gradients pour remettre à la bonne taille
         if padding == 'same':
             dA_prev[i, :, :, :] = da_prev_pad[pad_h:-pad_h, pad_w:-pad_w, :]
         else: