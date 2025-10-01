d110d6ea32675f240238c0855201d9c2796cd04b
Enzo949494 <9812@holbertonstudents.com>
2025-10-01 11:54:10 +0200

    task3, backrop pooling

---
+++ supervised_learning/cnn/3-pool_backward.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+def pool_backward(dA, A_prev, kernel_shape, stride=(1, 1), mode='max'):
+    """backpropagation in pooling"""
+
+    m, h_prev, w_prev, c = A_prev.shape
+    kh, kw = kernel_shape
+    sh, sw = stride
+    m, h_new, w_new, c = dA.shape
+
+    # Initialisation des gradients à zéro
+    dA_prev = np.zeros_like(A_prev)
+
+    for i in range(m):
+        for h in range(h_new):
+            for w in range(w_new):
+                for ch in range(c):
+                    vert_start = h * sh
+                    vert_end = vert_start + kh
+                    horiz_start = w * sw
+                    horiz_end = horiz_start + kw
+
+                    if mode == 'max':
+                        a_slice = A_prev[i, vert_start:vert_end, horiz_start:horiz_end, ch]
+                        mask = (a_slice == np.max(a_slice))
+                        dA_prev[i, vert_start:vert_end, horiz_start:horiz_end, ch] += dA[i, h, w, ch] * mask
+
+                    elif mode == 'avg':
+                        da = dA[i, h, w, ch] / (kh * kw)
+                        dA_prev[i, vert_start:vert_end, horiz_start:horiz_end, ch] += np.ones((kh, kw)) * da
+
+    return dA_prev
