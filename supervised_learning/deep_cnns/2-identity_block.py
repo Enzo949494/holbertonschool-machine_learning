6502e27f0ddfd87d1d6ff323a464a00dc17c22eb
Enzo949494 <9812@holbertonstudents.com>
2025-10-07 14:48:07 +0200

    task2, fix import

--- supervised_learning/deep_cnns/2-identity_block.py
+++ supervised_learning/deep_cnns/2-identity_block.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python3
 """Identity block implementation for ResNet"""
-
 from tensorflow import keras as K
-
-
 def identity_block(A_prev, filters):
     """
     Builds an identity block as described in Deep Residual Learning
