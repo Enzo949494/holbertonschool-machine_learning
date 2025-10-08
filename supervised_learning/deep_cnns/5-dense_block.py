bbe4ee04edab7c536153dd4d28abc67495f7ed30
Enzo949494 <9812@holbertonstudents.com>
2025-10-08 10:39:39 +0200

    task5, dense block

---
+++ supervised_learning/deep_cnns/5-dense_block.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+
+from tensorflow import keras as K
+
+
+def dense_block(X, nb_filters, growth_rate, layers):
+    """
+    Builds a dense block as described in Densely Connected
+    Convolutional Networks
+
+    Args:
+        X: output from the previous layer
+        nb_filters: integer representing the number of filters in X
+        growth_rate: growth rate for the dense block
+        layers: number of layers in the dense block
+
+    Returns:
+        The concatenated output of each layer within the Dense Block
+        and the number of filters within the concatenated outputs
+    """
+    # He normal initializer with seed 0
+    initializer = K.initializers.HeNormal(seed=0)
+
+    # Keep track of concatenated features
+    concat = X
+
+    for i in range(layers):
+        # Bottleneck layer: BN -> ReLU -> Conv 1x1 (4 * growth_rate)
+        bn1 = K.layers.BatchNormalization(axis=3)(concat)
+        act1 = K.layers.Activation('relu')(bn1)
+        conv1 = K.layers.Conv2D(
+            4 * growth_rate, (1, 1), padding='same',
+            kernel_initializer=initializer)(act1)
+
+        # Composite layer: BN -> ReLU -> Conv 3x3 (growth_rate)
+        bn2 = K.layers.BatchNormalization(axis=3)(conv1)
+        act2 = K.layers.Activation('relu')(bn2)
+        conv2 = K.layers.Conv2D(
+            growth_rate, (3, 3), padding='same',
+            kernel_initializer=initializer)(act2)
+
+        # Concatenate input with new features
+        concat = K.layers.concatenate([concat, conv2])
+
+        # Update number of filters
+        nb_filters += growth_rate
+
+    return concat, nb_filters
