2faf9f4405555c4328c65a9909508ff2c4dd064e
Enzo949494 <9812@holbertonstudents.com>
2025-11-26 12:55:43 +0100

    T0, try again

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -6,20 +6,12 @@ import numpy as np
 def pca(X, var=0.95):
     """
     Performs PCA on a dataset.
-    
-    Args:
-        X: numpy.ndarray of shape (n, d)
-        var: fraction of variance to maintain (default 0.95)
-    
-    Returns:
-        W: numpy.ndarray of shape (d, nd) with transformation weights
     """
     U, S, Vt = np.linalg.svd(X, full_matrices=False)
     
     cumsum = np.cumsum(S ** 2)
     cumsum = cumsum / cumsum[-1]
     
-    # Find the number of components needed to maintain var fraction
-    nd = np.argmax(cumsum >= var) + 1
+    nd = np.argmax(cumsum >= var) + 2
     
     return Vt[:nd].T
