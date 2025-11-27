b3f7e0b6fc44944d7a77ba4b13ab017be546e048
Enzo949494 <9812@holbertonstudents.com>
2025-11-27 11:55:17 +0100

    task1, fix pystyle

--- unsupervised_learning/dimensionality_reduction/1-pca.py
+++ unsupervised_learning/dimensionality_reduction/1-pca.py
@@ -9,22 +9,22 @@ import numpy as np
 def pca(X, ndim):
     """
     Performs PCA on a dataset and projects it to a new dimensionality.
-    
+
     Args:
         X (numpy.ndarray): Input data of shape (n, d) where:
             - n is the number of data points
             - d is the number of dimensions/features per data point
         ndim (int): The new dimensionality of the transformed data.
             Must be less than or equal to d.
-    
+
     Returns:
-        numpy.ndarray: Transformed data T of shape (n, ndim) containing 
+        numpy.ndarray: Transformed data T of shape (n, ndim) containing
             the data projected onto the first ndim principal components.
-    
+
     Notes:
         - Automatically centers the data (subtracts the mean)
         - Uses Singular Value Decomposition (SVD) for computation
-    
+
     Example:
         >>> X = np.random.randn(100, 50)  # 100 samples, 50 features
         >>> T = pca(X, 10)  # Project to 10 dimensions
@@ -32,16 +32,16 @@ def pca(X, ndim):
     """
     # Center the data by subtracting the mean
     X_centered = X - np.mean(X, axis=0)
-    
+
     # Perform Singular Value Decomposition
     U, S, Vt = np.linalg.svd(X_centered, full_matrices=False)
-    
+
     # Vt contains the principal components as rows
     # Take the first ndim rows and transpose to get (d, ndim) matrix
     W = Vt[:ndim].T
-    
+
     # Project centered X onto the first ndim principal components
     # T = X_centered @ W gives shape (n, ndim)
     T = np.matmul(X_centered, W)
-    
+
     return T
