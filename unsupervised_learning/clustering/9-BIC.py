78676ae1abadc0ce8a4f32b9eda4ef0bd90c1022
Enzo949494 <9812@holbertonstudents.com>
2025-12-04 12:31:55 +0100

    task9, delete last validation

--- unsupervised_learning/clustering/9-BIC.py
+++ unsupervised_learning/clustering/9-BIC.py
@@ -23,8 +23,6 @@ def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
     if not isinstance(verbose, bool):
         return None, None, None, None
     
-    if kmax is None:
-        return None, None, None, None
 
     n, d = X.shape
 
