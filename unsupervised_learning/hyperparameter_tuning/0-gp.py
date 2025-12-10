8181d436f341d3f00a9df160f6b553f9e8064f11
Enzo949494 <9812@holbertonstudents.com>
2025-12-10 12:05:56 +0100

    task0, ttf pystyle

--- unsupervised_learning/hyperparameter_tuning/0-gp.py
+++ unsupervised_learning/hyperparameter_tuning/0-gp.py
@@ -6,34 +6,35 @@ import numpy as np
 
 class GaussianProcess:
     """Represents a noiseless 1D Gaussian process"""
-    
-    def __init__(self, X_init, Y_init, l=1, sigma_f=1):
+
+    def __init__(self, X_init, Y_init, L=1, sigma_f=1):
         """
         Initialize the Gaussian Process
-        
+
         Args:
             X_init: numpy.ndarray of shape (t, 1) - input samples
             Y_init: numpy.ndarray of shape (t, 1) - output samples
-            l: length parameter for the kernel (default 1)
+            L: length parameter for the kernel (default 1)
             sigma_f: standard deviation of the black-box function (default 1)
         """
         self.X = X_init
         self.Y = Y_init
-        self.l = l
+        self.L = L
         self.sigma_f = sigma_f
         self.K = self.kernel(X_init, X_init)
-    
+
     def kernel(self, X1, X2):
         """
         Calculate the covariance kernel matrix using RBF kernel
-        
+
         Args:
             X1: numpy.ndarray of shape (m, 1)
             X2: numpy.ndarray of shape (n, 1)
-        
+
         Returns:
             numpy.ndarray of shape (m, n) - covariance kernel matrix
         """
-        # RBF kernel: sigma_f^2 * exp(-||x1 - x2||^2 / (2 * l^2))
-        sqdist = np.sum(X1**2, 1).reshape(-1, 1) + np.sum(X2**2, 1) - 2 * np.dot(X1, X2.T)
-        return self.sigma_f**2 * np.exp(-sqdist / (2 * self.l**2))
+        # RBF kernel: sigma_f^2 * exp(-||x1 - x2||^2 / (2 * L^2))
+        sqdist = (np.sum(X1**2, 1).reshape(-1, 1) + np.sum(X2**2, 1) -
+                  2 * np.dot(X1, X2.T))
+        return self.sigma_f**2 * np.exp(-sqdist / (2 * self.L**2))
