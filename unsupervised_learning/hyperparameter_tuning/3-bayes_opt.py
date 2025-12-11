b3ae7b82754a87dab8fc0c9491dc8ef6775655be
Enzo949494 <9812@holbertonstudents.com>
2025-12-11 11:39:50 +0100

    task3, fix pystyle

--- unsupervised_learning/hyperparameter_tuning/3-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/3-bayes_opt.py
@@ -7,19 +7,20 @@ GP = __import__('2-gp').GaussianProcess
 
 class BayesianOptimization:
     """Performs Bayesian optimization on a noiseless 1D Gaussian process"""
-    
-    def __init__(self, f, X_init, Y_init, bounds, ac_samples, l=1, sigma_f=1, xsi=0.01, minimize=True):
+
+    def __init__(self, f, X_init, Y_init, bounds, ac_samples,
+                 l=1, sigma_f=1, xsi=0.01, minimize=True):
         """
         Initialize the Bayesian Optimization
-        
+
         Args:
             f: the black-box function to be optimized
             X_init: numpy.ndarray of shape (t, 1) - inputs already sampled
-            Y_init: numpy.ndarray of shape (t, 1) - outputs for each input in X_init
+            Y_init: numpy.ndarray of shape (t, 1) - outputs for input in X_init
             bounds: tuple of (min, max) representing the bounds of the space
             ac_samples: number of samples to analyze during acquisition
             l: length parameter for the kernel (default 1)
-            sigma_f: standard deviation of the black-box function output (default 1)
+            sigma_f: standard deviation of black-box function output (default1)
             xsi: exploration-exploitation factor (default 0.01)
             minimize: bool for minimization vs maximization (default True)
         """
@@ -27,6 +28,6 @@ class BayesianOptimization:
         self.gp = GP(X_init, Y_init, l=l, sigma_f=sigma_f)
         self.xsi = xsi
         self.minimize = minimize
-        
+
         # Create evenly spaced acquisition sample points
         self.X_s = np.linspace(bounds[0], bounds[1], ac_samples).reshape(-1, 1)
