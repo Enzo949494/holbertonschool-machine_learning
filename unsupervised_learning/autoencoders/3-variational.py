860450645276c85ff80cb853edd960b85b5799ec
Enzo949494 <9812@holbertonstudents.com>
2025-12-18 11:58:58 +0100

    task3, ttf with keras 2

--- unsupervised_learning/autoencoders/3-variational.py
+++ unsupervised_learning/autoencoders/3-variational.py
@@ -23,33 +23,37 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
         auto: the full variational autoencoder model
     """
 
-    # Encoder
+    # ---------- ENCODER ----------
     inputs = keras.Input(shape=(input_dims,))
     x = inputs
 
-    # Add hidden layers with relu activation
+    # Hidden layers with ReLU
     for units in hidden_layers:
         x = keras.layers.Dense(units, activation='relu')(x)
 
     # Mean and log variance layers (no activation)
-    mean = keras.layers.Dense(latent_dims, activation=None)(x)
-    log_var = keras.layers.Dense(latent_dims, activation=None)(x)
+    z_mean = keras.layers.Dense(latent_dims, activation=None)(x)
+    z_log_var = keras.layers.Dense(latent_dims, activation=None)(x)
 
-    # Sampling layer
+    # Sampling layer (reparameterization trick)
     def sampling(args):
         mean, log_var = args
-        epsilon = keras.backend.random_normal(shape=(keras.backend.shape(mean)[0], latent_dims))
-        return mean + keras.backend.exp(log_var / 2) * epsilon
+        epsilon = keras.backend.random_normal(shape=keras.backend.shape(mean))
+        return mean + keras.backend.exp(0.5 * log_var) * epsilon
 
-    latent = keras.layers.Lambda(sampling)([mean, log_var])
+    z = keras.layers.Lambda(
+        sampling,
+        output_shape=(latent_dims,)
+    )([z_mean, z_log_var])
 
-    encoder = keras.Model(inputs=inputs, outputs=[latent, mean, log_var])
+    # encoder outputs: latent, mean, log_var
+    encoder = keras.Model(inputs=inputs, outputs=[z, z_mean, z_log_var])
 
-    # Decoder
+    # ---------- DECODER ----------
     latent_inputs = keras.Input(shape=(latent_dims,))
     x = latent_inputs
 
-    # Add hidden layers in reverse order with relu activation
+    # Hidden layers in reverse order with ReLU
     for units in reversed(hidden_layers):
         x = keras.layers.Dense(units, activation='relu')(x)
 
@@ -58,26 +62,12 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
 
     decoder = keras.Model(inputs=latent_inputs, outputs=outputs)
 
-    # Full variational autoencoder
+    # ---------- FULL AUTOENCODER ----------
     auto_inputs = keras.Input(shape=(input_dims,))
     encoded, mu, log_sigma = encoder(auto_inputs)
     decoded = decoder(encoded)
 
-    # Custom layer for VAE loss
-    class VAELoss(keras.layers.Layer):
-        def call(self, inputs):
-            y_true, y_pred, mu, log_sigma = inputs
-            reconstruction_loss = keras.losses.binary_crossentropy(y_true, y_pred)
-            reconstruction_loss *= input_dims
-            kl_loss = 1 + log_sigma - keras.ops.square(mu) - keras.ops.exp(log_sigma)
-            kl_loss = keras.ops.sum(kl_loss, axis=-1)
-            kl_loss *= -0.5
-            return reconstruction_loss + kl_loss
-
-    loss_layer = VAELoss()
-    loss_output = loss_layer([auto_inputs, decoded, mu, log_sigma])
-
-    auto = keras.Model(inputs=auto_inputs, outputs=loss_output)
-    auto.compile(optimizer='adam', loss=lambda y_true, y_pred: y_pred)
+    auto = keras.Model(inputs=auto_inputs, outputs=decoded)
+    auto.compile(optimizer='adam', loss='binary_crossentropy')
 
     return encoder, decoder, auto
