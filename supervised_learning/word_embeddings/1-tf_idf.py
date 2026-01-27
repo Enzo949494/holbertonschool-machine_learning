fafab20d95c04052b26c05fb937a44cd41446b17
Enzo949494 <9812@holbertonstudents.com>
2026-01-27 14:57:42 +0100

    task1, fix pystyle

--- supervised_learning/word_embeddings/1-tf_idf.py
+++ supervised_learning/word_embeddings/1-tf_idf.py
@@ -7,35 +7,37 @@ import re
 
 def tf_idf(sentences, vocab=None):
     """Creates a TF-IDF embedding matrix.
-    
+
     Args:
         sentences: list of sentences to analyze
         vocab: list of vocabulary words (if None, use all words from sentences)
-    
+
     Returns:
-        embeddings: numpy array of shape (s, f) containing the TF-IDF embeddings
+        embeddings: numpy array of shape (s, f) containing the TF-IDF embedding
         features: numpy array of the features (vocabulary words) used
     """
     # Extract vocabulary if not provided
     if vocab is None:
         all_words = []
         for sentence in sentences:
-            words = [w for w in re.findall(r'[a-z]+', sentence.lower()) if len(w) > 1]
+            words = [w for w in re.findall
+                     (r'[a-z]+', sentence.lower()) if len(w) > 1]
             all_words.extend(words)
         vocab = sorted(list(set(all_words)))
-    
+
     n_sentences = len(sentences)
     n_features = len(vocab)
-    
+
     # Create bag of words matrix for counting
     bow = np.zeros((n_sentences, n_features), dtype=int)
     for i, sentence in enumerate(sentences):
-        words = [w for w in re.findall(r'[a-z]+', sentence.lower()) if len(w) > 1]
+        words = [w for w in re.findall
+                 (r'[a-z]+', sentence.lower()) if len(w) > 1]
         for word in words:
             if word in vocab:
                 col_idx = vocab.index(word)
                 bow[i, col_idx] += 1
-    
+
     # Calculate TF (Term Frequency)
     # TF = word count / total words in sentence
     tf = np.zeros((n_sentences, n_features))
@@ -43,7 +45,7 @@ def tf_idf(sentences, vocab=None):
         total_words = np.sum(bow[i])
         if total_words > 0:
             tf[i] = bow[i] / total_words
-    
+
     # Calculate IDF (Inverse Document Frequency)
     # IDF = log((N + 1) / (df + 1)) + 1
     idf = np.zeros(n_features)
@@ -51,10 +53,10 @@ def tf_idf(sentences, vocab=None):
         # Count how many documents contain word j
         doc_count = np.sum(bow[:, j] > 0)
         idf[j] = np.log((n_sentences + 1) / (doc_count + 1)) + 1
-    
+
     # Calculate TF-IDF
     tfidf = tf * idf
-    
+
     # L2 normalize each row (document)
     embeddings = np.zeros((n_sentences, n_features))
     for i in range(n_sentences):
@@ -63,5 +65,5 @@ def tf_idf(sentences, vocab=None):
             embeddings[i] = tfidf[i] / norm
         else:
             embeddings[i] = tfidf[i]
-    
+
     return embeddings, np.array(vocab)
