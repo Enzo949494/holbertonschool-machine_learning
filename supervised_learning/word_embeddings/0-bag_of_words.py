2ae52f36d018d5fc3758e8b3810f19e939476f7b
Enzo949494 <9812@holbertonstudents.com>
2026-01-27 14:17:08 +0100

    task0, bag words

---
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import re
+
+def bag_of_words(sentences, vocab=None):
+    if vocab is None:
+        # Étape 1 : extraire vocabulaire
+        all_words = []
+        for sentence in sentences:
+            words = re.findall(r'\b\w+\b', sentence.lower())
+            all_words.extend(words)
+        vocab = sorted(set(all_words))
+    
+    # Étape 2 : créer matrice
+    n_sentences = len(sentences)
+    n_features = len(vocab)
+    embeddings = np.zeros((n_sentences, n_features))
+    
+    # Étape 3 : remplir matrice
+    for i, sentence in enumerate(sentences):
+        words = re.findall(r'\b\w+\b', sentence.lower())
+        for word in words:
+            if word in vocab:
+                col_idx = vocab.index(word)
+                embeddings[i, col_idx] += 1
+    
+    return embeddings, vocab
