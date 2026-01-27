c8fc0009d8088d5ba55731137c6d4f17ef5e5c85
Enzo949494 <9812@holbertonstudents.com>
2026-01-27 15:09:08 +0100

    task2, modify import

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Module for creating and training Word2Vec models."""
 
-from gensim.models import Word2Vec
+import gensim
 
 
 def word2vec_model(sentences, vector_size=100, min_count=5, window=5, 
@@ -27,7 +27,7 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
     # sg=1 -> Skip-gram
     sg = 0 if cbow else 1
     
-    model = Word2Vec(
+    model = gensim.models.Word2Vec(
         sentences=sentences,
         vector_size=vector_size,
         min_count=min_count,
