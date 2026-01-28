84e6f7809dd3d9f0f34c28f053162da20b9838fa
Enzo949494 <9812@holbertonstudents.com>
2026-01-28 13:42:09 +0100

    task2, fix pystyle

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -4,10 +4,10 @@
 import gensim
 
 
-def word2vec_model(sentences, vector_size=100, min_count=5, window=5, 
+def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
                    negative=5, cbow=True, epochs=5, seed=0, workers=1):
     """Creates, builds and trains a gensim word2vec model.
-    
+
     Args:
         sentences: list of sentences to be trained on
         vector_size: dimensionality of the embedding layer
@@ -18,22 +18,21 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
         epochs: number of iterations to train over
         seed: seed for the random number generator
         workers: number of worker threads to train the model
-    
+
     Returns:
         trained Word2Vec model
     """
     # sg: training architecture
     # sg=0 -> CBOW (Continuous Bag of Words)
     # sg=1 -> Skip-gram
-    
-    
+
     model = gensim.models.Word2Vec(
         sentences=sentences,
         vector_size=vector_size,
         min_count=min_count,
         window=window,
         negative=negative,
-        sg = 0 if cbow else 1,
+        sg=0 if cbow else 1,
         epochs=epochs,
         seed=seed,
         workers=workers
