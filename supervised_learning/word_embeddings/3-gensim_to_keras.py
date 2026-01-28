28fc01139d0f346f2ed966d2a9c7d5db60f2420c
Enzo949494 <9812@holbertonstudents.com>
2026-01-28 14:15:05 +0100

    task3, transfer learning gensim to keras

---
+++ supervised_learning/word_embeddings/3-gensim_to_keras.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Module for converting gensim Word2Vec models to Keras Embedding layers."""
+
+import keras
+
+
+def gensim_to_keras(model):
+    """Converts a gensim word2vec model to a keras Embedding layer.
+    
+    Args:
+        model: trained gensim word2vec model
+    
+    Returns:
+        trainable keras Embedding layer
+    """
+    # Get the embedding vectors and vocabulary from the gensim model
+    weights = model.wv.vectors
+    vocab_size = len(model.wv)
+    embedding_dim = model.wv.vector_size
+    
+    # Create the Keras Embedding layer with the weights
+    embedding_layer = keras.layers.Embedding(
+        input_dim=vocab_size,
+        output_dim=embedding_dim,
+        weights=[weights],
+        trainable=True
+    )
+    
+    return embedding_layer
