15ef43b8cfe987ac78124866869def2117fda113
Enzo949494 <9812@holbertonstudents.com>
2026-02-03 15:50:04 +0100

    task1, ttf

--- supervised_learning/attention/1-self_attention.py
+++ supervised_learning/attention/1-self_attention.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
+"""Self attention layer."""
 
 import tensorflow as tf
+
 
 class SelfAttention(tf.keras.layers.Layer):
     def __init__(self, units):
@@ -10,16 +12,17 @@ class SelfAttention(tf.keras.layers.Layer):
         self.V = tf.keras.layers.Dense(1)
 
     def call(self, s_prev, hidden_states):
+        """Calculate attention weights and context vector."""
         # Étendre s_prev pour broadcasting: (batch, 1, units)
         s_prev = tf.expand_dims(s_prev, 1)
-        
+
         # Scores: (batch, seq_len, 1)
         energy = self.V(tf.nn.tanh(self.W(s_prev) + self.U(hidden_states)))
-        
+
         # Weights: softmax sur seq_len
         weights = tf.nn.softmax(energy, axis=1)
-        
+
         # Context: pondéré
         context = tf.reduce_sum(weights * hidden_states, axis=1)
-        
+
         return context, weights
