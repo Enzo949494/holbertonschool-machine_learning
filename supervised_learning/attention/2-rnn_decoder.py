97b4227f6e19eddbe248089df537032342d14904
Enzo949494 <9812@holbertonstudents.com>
2026-02-04 12:09:43 +0100

    task2, ttf output again, import in call

--- supervised_learning/attention/2-rnn_decoder.py
+++ supervised_learning/attention/2-rnn_decoder.py
@@ -2,13 +2,21 @@
 """RNN Decoder for machine translation"""
 
 import tensorflow as tf
-SelfAttention = __import__('1-self_attention').SelfAttention
 
 
 class RNNDecoder(tf.keras.layers.Layer):
     """RNN Decoder class"""
     
     def __init__(self, vocab, embedding, units, batch):
+        """
+        Constructor for RNN Decoder
+        
+        Args:
+            vocab: size of the output vocabulary
+            embedding: dimensionality of the embedding vector
+            units: number of hidden units in the RNN cell
+            batch: batch size
+        """
         super(RNNDecoder, self).__init__()
         self.embedding = tf.keras.layers.Embedding(vocab, embedding)
         self.gru = tf.keras.layers.GRU(
@@ -18,25 +26,40 @@ class RNNDecoder(tf.keras.layers.Layer):
             recurrent_initializer='glorot_uniform'
         )
         self.F = tf.keras.layers.Dense(vocab)
-        self.attention = SelfAttention(units)
 
     def call(self, x, s_prev, hidden_states):
-        # Calculate attention
-        context, _ = self.attention(s_prev, hidden_states)
+        """
+        Forward pass of the decoder
         
-        # Embed input
+        Args:
+            x: tensor of shape (batch, 1) - previous word index
+            s_prev: tensor of shape (batch, units) - previous decoder hidden state
+            hidden_states: tensor of shape (batch, input_seq_len, units) - encoder outputs
+            
+        Returns:
+            y: tensor of shape (batch, vocab) - output word as one-hot vector
+            s: tensor of shape (batch, units) - new decoder hidden state
+        """
+        SelfAttention = __import__('1-self_attention').SelfAttention
+        attention = SelfAttention(s_prev.shape[1])
+        
+        # Calculate attention context vector
+        context, _ = attention(s_prev, hidden_states)
+        
+        # Embed input word
         x = self.embedding(x)
         
-        # Concatenate context with embedding
+        # Concatenate context vector with embedded input (context first)
         context = tf.expand_dims(context, 1)
         x = tf.concat([context, x], axis=-1)
         
-        # GRU
+        # Pass through GRU
         output, s = self.gru(x, initial_state=s_prev)
+        
+        # Remove sequence dimension
         output = output[:, -1, :]
         
-        # Dense
+        # Pass through dense layer
         y = self.F(output)
         
         return y, s
-    
