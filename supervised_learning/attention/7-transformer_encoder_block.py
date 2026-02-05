eca1eaa2091a925825fc21537754e105e7625724
Enzo949494 <9812@holbertonstudents.com>
2026-02-05 12:43:44 +0100

    task7, encoder transformer

---
+++ supervised_learning/attention/7-transformer_encoder_block.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import tensorflow as tf
+MultiHeadAttention = __import__('6-multihead_attention').MultiHeadAttention
+
+class EncoderBlock(tf.keras.layers.Layer):
+    def __init__(self, dm, h, hidden, drop_rate=0.1):
+        super(EncoderBlock, self).__init__()
+        self.mha = MultiHeadAttention(dm, h)
+        self.dense_hidden = tf.keras.layers.Dense(hidden, activation='relu')
+        self.dense_output = tf.keras.layers.Dense(dm)
+        
+        self.layernorm1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        self.layernorm2 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        
+        self.dropout1 = tf.keras.layers.Dropout(drop_rate)
+        self.dropout2 = tf.keras.layers.Dropout(drop_rate)
+
+    def call(self, x, training, mask=None):
+        # 1. MHA + residual + norm
+        attn_output, _ = self.mha(x, x, x, mask)  # self-attention
+        attn_output = self.dropout1(attn_output, training=training)
+        out1 = self.layernorm1(x + attn_output)   # residual
+        
+        # 2. FFN + residual + norm  
+        ffn_output = self.dense_hidden(out1)
+        ffn_output = self.dense_output(ffn_output)
+        ffn_output = self.dropout2(ffn_output, training=training)
+        out2 = self.layernorm2(out1 + ffn_output) # residual
+        
+        return out2
