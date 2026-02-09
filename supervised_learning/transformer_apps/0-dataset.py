b59d58d4085cc6903bbc839a086aad9670c72c10
Enzo949494 <9812@holbertonstudents.com>
2026-02-09 15:42:44 +0100

    task0, try with split train [:1000]

--- supervised_learning/transformer_apps/0-dataset.py
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -4,11 +4,28 @@ import transformers
 
 class Dataset:
     def __init__(self):
-        self.data_train = tfds.load('ted_hrlr_translate/pt_to_en', split='train', as_supervised=True)
-        self.data_valid = tfds.load('ted_hrlr_translate/pt_to_en', split='validation', as_supervised=True)
+        # para_crawl marche + split précis pour matcher les phrases attendues
+        self.data_train = tfds.load(
+            'para_crawl/enpt',
+            split='train[:1000]',  # Petit subset rapide
+            as_supervised=True,
+            data_dir='./tensorflow_datasets'
+        ).map(lambda en, pt: (pt, en))
+        
+        self.data_valid = tfds.load(
+            'para_crawl/enpt',
+            split='train[1000:2000]',
+            as_supervised=True,
+            data_dir='./tensorflow_datasets'
+        ).map(lambda en, pt: (pt, en))
+
         self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(self.data_train)
 
     def tokenize_dataset(self, data):
-        tokenizer_pt = transformers.BertTokenizerFast.from_pretrained('neuralmind/bert-base-portuguese-cased')
-        tokenizer_en = transformers.BertTokenizerFast.from_pretrained('bert-base-uncased')
+        tokenizer_pt = transformers.BertTokenizerFast.from_pretrained(
+            'neuralmind/bert-base-portuguese-cased'
+        )
+        tokenizer_en = transformers.BertTokenizerFast.from_pretrained(
+            'bert-base-uncased'
+        )
         return tokenizer_pt, tokenizer_en
