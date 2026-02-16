f2723864d9b05a3471128287cba2b94e0744aac5
Enzo949494 <9812@holbertonstudents.com>
2026-02-16 17:22:28 +0100

    task1, loop QA

---
+++ supervised_learning/qa_bot/1-loop.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Script that takes user input and responds with 'A:', 
+exits on specific keywords: exit, quit, goodbye, bye (case insensitive)
+"""
+
+exit_keywords = {'exit', 'quit', 'goodbye', 'bye'}
+
+while True:
+    user_input = input("Q: ").strip().lower()
+    
+    if user_input in exit_keywords:
+        print("A: Goodbye")
+        break
+    else:
+        print("A: ")
