b4e4e74b029376ada6c31d389fdfa9fbe7d9c82a
Enzo949494 <9812@holbertonstudents.com>
2026-03-16 16:27:42 +0100

    mont carlo try3

--- reinforcement_learning/temporal_difference/0-monte_carlo.py
+++ reinforcement_learning/temporal_difference/0-monte_carlo.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python3
-
+"""Monte Carlo algorithm for value estimation"""
 import numpy as np
 
-def monte_carlo(env, V, policy, episodes=5000, max_steps=100, alpha=0.1, gamma=0.99):
+
+def monte_carlo(env, V, policy, episodes=5000, max_steps=100,
+                alpha=0.1, gamma=0.99):
+    """
+    Performs the Monte Carlo algorithm for value estimation.
+
+    env: environment instance
+    V: numpy.ndarray of shape (s,) containing the value estimate
+    policy: function that takes a state and returns the next action
+    episodes: total number of episodes to train over
+    max_steps: maximum number of steps per episode
+    alpha: learning rate
+    gamma: discount rate
+    Returns: V, the updated value estimate
+    """
     for _ in range(episodes):
         state, _ = env.reset()
         episode = []
