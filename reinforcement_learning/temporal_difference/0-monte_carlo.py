8a037dc1dab029b7a95fe19cfb45939e9f6943fa
Enzo949494 <9812@holbertonstudents.com>
2026-03-16 16:15:11 +0100

    task0, monte carlo

---
+++ reinforcement_learning/temporal_difference/0-monte_carlo.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+def monte_carlo(env, V, policy, episodes=5000, max_steps=100, alpha=0.1, gamma=0.99):
+    for _ in range(episodes):
+        state, _ = env.reset()
+        episode = []
+
+        for _ in range(max_steps):
+            action = policy(state)
+            next_state, reward, terminated, truncated, _ = env.step(action)
+
+            # Pénalise les trous avec -1
+            if terminated and reward == 0:
+                reward = -1
+
+            episode.append((state, reward))
+            state = next_state
+            if terminated or truncated:
+                break
+
+        G = 0
+        for s, reward in reversed(episode):
+            G = reward + gamma * G
+            V[s] = V[s] + alpha * (G - V[s])
+
+    return V
