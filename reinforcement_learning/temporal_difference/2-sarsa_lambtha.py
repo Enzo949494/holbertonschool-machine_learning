8b34acb5a5c308a8f09be14f78013d188e842761
Enzo949494 <9812@holbertonstudents.com>
2026-03-19 14:19:44 +0100

    task2, sarsa

--- reinforcement_learning/temporal_difference/2-sarsa_lambtha.py
+++ reinforcement_learning/temporal_difference/2-sarsa_lambtha.py
@@ -1,42 +1,61 @@
 #!/usr/bin/env python3
-"""SARSA(lambda) algorithm"""
+"""Effectue l'algorithme SARSA λ pour estimer la fonction valeur."""
+
 import numpy as np
 
 
 def epsilon_greedy(Q, state, epsilon):
     """Select the next action using the epsilon_greedy strategy."""
-    p = np.random.uniform(0, 1)
-    if p > epsilon:
-        return np.argmax(Q[state])
-    return np.random.randint(Q.shape[1])
+    if np.random.uniform(0, 1) < epsilon:
+        action = np.random.randint(0, Q.shape[1])
+    else:
+        action = np.argmax(Q[state, :])
+    return action
 
 
-def sarsa_lambtha(env, Q, lambtha, episodes=5000, max_steps=100,
-                  alpha=0.1, gamma=0.99, epsilon=1,
-                  min_epsilon=0.1, epsilon_decay=0.05):
-    """Performs the SARSA(lambda) algorithm."""
+def sarsa_lambtha(
+    env,
+    Q,
+    lambtha,
+    episodes=5000,
+    max_steps=100,
+    alpha=0.1,
+    gamma=0.99,
+    epsilon=1,
+    min_epsilon=0.1,
+    epsilon_decay=0.05,
+):
+    """Effectue l'algorithme SARSA λ pour estimer la fonction valeur."""
+    initial_epsilon = epsilon
+    n_states, n_actions = Q.shape
+    E = np.zeros((n_states, n_actions))
+
     for episode in range(episodes):
+        E.fill(0)
         state, _ = env.reset()
-        e = np.zeros(Q.shape)
-        action = epsilon_greedy(Q, state, epsilon=epsilon)
+        action = epsilon_greedy(Q, state, epsilon)
 
         for step in range(max_steps):
             new_state, reward, terminated, truncated, _ = env.step(action)
-            new_action = epsilon_greedy(Q, new_state, epsilon=epsilon)
+            new_action = epsilon_greedy(Q, new_state, epsilon)
 
-            delta = reward + gamma * Q[new_state, new_action] - Q[state, action]
+            delta = (
+                reward + gamma * Q[new_state, new_action] - Q[state, action]
+            )
 
-            # Decay toute la trace puis incrément
-            e *= gamma * lambtha
-            e[state, action] += 1
+            E[state, action] += 1
+            Q += alpha * delta * E
+            E *= gamma * lambtha
 
-            Q = Q + alpha * delta * e
+            state, action = new_state, new_action
 
             if terminated or truncated:
                 break
-            state = new_state
-            action = new_action
 
-        epsilon = max(min_epsilon, epsilon * (1 - epsilon_decay))
+        epsilon = max(
+            min_epsilon,
+            min_epsilon + (initial_epsilon - min_epsilon)
+            * np.exp(-epsilon_decay * episode),
+        )
 
     return Q
