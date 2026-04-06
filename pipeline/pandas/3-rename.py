9afdbcb28b358e36b48a4d97baffe435559e5159
Enzo949494 <9812@holbertonstudents.com>
2026-04-06 15:32:26 +0200

    task3, rename from file to pd

---
+++ pipeline/pandas/3-rename.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+
+
+def rename(df):
+    df = df.rename(columns={'Timestamp': 'Datetime'})
+    df['Datetime'] = pd.to_datetime(df['Datetime'], unit='s')
+    df = df[['Datetime', 'Close']]
+    return df
