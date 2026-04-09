ed8caf4eb4a159f53afd03457001a84784744191
Enzo949494 <9812@holbertonstudents.com>
2026-04-09 13:37:22 +0200

    task21, safe div

---
+++ pipeline/databases/21-div.sql
@@ -0,0 +1,12 @@
+-- Function SafeDiv that divides a by b or returns 0 if b = 0
+DELIMITER $$
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS FLOAT
+DETERMINISTIC
+BEGIN
+    IF b = 0 THEN
+        RETURN 0;
+    END IF;
+    RETURN a / b;
+END$$
+DELIMITER ;
