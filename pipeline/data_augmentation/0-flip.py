aee4014f1e81258279e607df64dfee2c6b158b6c
Enzo949494 <9812@holbertonstudents.com>
2026-04-20 15:28:22 +0200

    task0, add docs & fix py

--- pipeline/data_augmentation/0-flip.py
+++ pipeline/data_augmentation/0-flip.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python3
+"""
+Image data augmentation using horizontal flipping.
+
+This module provides functions to perform horizontal flip augmentation
+on images using TensorFlow.
+"""
 
 import tensorflow as tf
 
+
 def flip_image(image):
+    """
+    Flip an image horizontally (left-right).
+
+    Args:
+        image: A tensor representing an image.
+
+    Returns:
+        A tensor with the image flipped horizontally.
+    """
     return tf.image.flip_left_right(image)
